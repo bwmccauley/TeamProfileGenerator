f3d15d2148a1603a347fae736ae6cd42baa7e104
bwmccauley <brandonmccauley@Brandons-MacBook-Pro.local>
2023-01-08 12:59:39 -0500

    added parent and chilc classes

---
+++ tests/lib/Employee.js
@@ -0,0 +1,32 @@
+// Employee class is parent of all Role classes
+class Employee {
+    constructor(name, id, email, imgSrc) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.imgSrc = imgSrc;
+        this.title = 'Employee';
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getImgSrc() {
+        return this.imgSrc;
+    }
+
+    getRole() {
+        return this.title;
+    }
+}
+
+module.exports = Employee;
